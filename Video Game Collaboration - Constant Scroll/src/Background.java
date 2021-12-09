cf57c3e2baf914b5574c3303d7f42013b9e35391
s017769 <s017769@lm-24s017769.lmsd.org>
2021-12-09 12:56:59 -0500

    Always running and can't go above screen height

--- Video Game Collaboration - Constant Scroll/src/Background.java
+++ Video Game Collaboration - Constant Scroll/src/Background.java
@@ -12,59 +12,59 @@ import javax.swing.ImageIcon;
 
 public class Background {
 
-protected ImageIcon image;
-protected int scale;
-protected int x;
+	protected ImageIcon image;
+	protected int scale;
+	protected int x;
 
-public Background(int x) {
-ClassLoader cldr = this.getClass().getClassLoader(); // These five lines of code load the background picture.
-String imagePath = "images/background/raceTrack.png"; // Change this line if you want to use a different
-URL imageURL = cldr.getResource(imagePath); // background image.  The image should be saved in the
-scale = 1;
+	public Background(int x) {
+		ClassLoader cldr = this.getClass().getClassLoader(); // These five lines of code load the background picture.
+		String imagePath = "images/background/raceTrack.png"; // Change this line if you want to use a different
+		URL imageURL = cldr.getResource(imagePath); // background image.  The image should be saved in the
+		scale = 1;
 
-image = new ImageIcon(imageURL);
+		image = new ImageIcon(imageURL);
 
-Image scaled = image.getImage().getScaledInstance(image.getIconWidth() / scale,
-image.getIconHeight() / scale, Image.SCALE_SMOOTH);
+		Image scaled = image.getImage().getScaledInstance(image.getIconWidth() / scale,
+				image.getIconHeight() / scale, Image.SCALE_SMOOTH);
 
-image = new ImageIcon(scaled);
+		image = new ImageIcon(scaled);
 
-this.x = x;
-}
+		this.x = x;
+	}
 
-public Background() {
-this(0);
-}
+	public Background() {
+		this(0);
+	}
 
-public void draw(Component c, Graphics g) {
-image.paintIcon(c, g, x, 0);
-}
+	public void draw(Component c, Graphics g) {
+		image.paintIcon(c, g, x, 0);
+	}
 
-public int getHeight() {
-return image.getIconHeight();
-}
+	public int getHeight() {
+		return image.getIconHeight();
+	}
 
-public int getWidth() {
-return image.getIconWidth();
-}
+	public int getWidth() {
+		return image.getIconWidth();
+	}
 
-public int getX() {
-return x;
-}
+	public int getX() {
+		return x;
+	}
 
-public void move() {
-if(getX()>-image.getIconWidth())
-x--;
-else
-reset();
-}
+	public void move() {
+		if(getX()>-image.getIconWidth())
+			x--;
+		else
+			reset();
+	}
 
-public void reset() {
-x = image.getIconWidth();
-}
+	public void reset() {
+		x = image.getIconWidth();
+	}
 
-public ImageIcon getImage() {
-return image;
-}
+	public ImageIcon getImage() {
+		return image;
+	}
 
 }
--- Video Game Collaboration - Constant Scroll/src/GraphicsPanel.java
+++ Video Game Collaboration - Constant Scroll/src/GraphicsPanel.java
@@ -28,174 +28,178 @@ import java.awt.Rectangle;
 
 public class GraphicsPanel extends JPanel implements KeyListener{
 
-private Timer timer; // The timer is used to move objects at a consistent time interval.
-private Boolean mover;
-private Background background1; // The background object will display a picture in the background.
-private Background background2; // There has to be two background objects for scrolling.
-private ArrayList<Item> cans;
-private Sprite sprite; // create a Sprite object
-private Item item;
-private int counter;
-// This declares an Item object. You can make a Item display
-// pretty much any image that you would like by passing it
-// the path for the image.
+	private Timer timer; // The timer is used to move objects at a consistent time interval.
+	private Boolean mover;
+	private Background background1; // The background object will display a picture in the background.
+	private Background background2; // There has to be two background objects for scrolling.
+	private ArrayList<Item> cans;
+	private Sprite sprite; // create a Sprite object
+	private Item item;
+	private int counter;
+	// This declares an Item object. You can make a Item display
+	// pretty much any image that you would like by passing it
+	// the path for the image.
 
 
-public GraphicsPanel(){
-background1 = new Background(); // You can set the background variable equal to an instance of any of  
-background2 = new Background(-background1.getImage().getIconWidth());
-mover = true;
-item = new Item(500, 200, "images/objects/box.png", 4);  
-// The Item constructor has 4 parameters - the x coordinate, y coordinate
-// the path for the image, and the scale. The scale is used to make the
-// image smaller, so the bigger the scale, the smaller the image will be.
-counter = 0;
-cans = new ArrayList<Item>();
-sprite = new Sprite(background1.getWidth()/2-190, background1.getHeight()/2-60);
-// The Sprite constuctor has two parameter - - the x coordinate and y coordinate
+	public GraphicsPanel(){
+		background1 = new Background(); // You can set the background variable equal to an instance of any of  
+		background2 = new Background(-background1.getImage().getIconWidth());
+		mover = true;
+		item = new Item(500, 200, "images/objects/box.png", 4);  
+		// The Item constructor has 4 parameters - the x coordinate, y coordinate
+		// the path for the image, and the scale. The scale is used to make the
+		// image smaller, so the bigger the scale, the smaller the image will be.
+		counter = 0;
+		cans = new ArrayList<Item>();
+		sprite = new Sprite(background1.getWidth()/2-190, background1.getHeight()/2-60);
+		// The Sprite constuctor has two parameter - - the x coordinate and y coordinate
 
-setPreferredSize(new Dimension(background1.getImage().getIconWidth(),
-background2.getImage().getIconHeight()));  
-// This line of code sets the dimension of the panel equal to the dimensions
-// of the background image.
+		setPreferredSize(new Dimension(background1.getImage().getIconWidth(),
+				background2.getImage().getIconHeight()));  
+		// This line of code sets the dimension of the panel equal to the dimensions
+		// of the background image.
 
-timer = new Timer(5, new ClockListener(this));   // This object will call the ClockListener's
-// action performed method every 5 milliseconds once the
-// timer is started. You can change how frequently this
-// method is called by changing the first parameter.
-timer.start();
-this.setFocusable(true);     // for keylistener
-this.addKeyListener(this);
+		timer = new Timer(5, new ClockListener(this));   // This object will call the ClockListener's
+		// action performed method every 5 milliseconds once the
+		// timer is started. You can change how frequently this
+		// method is called by changing the first parameter.
+		timer.start();
+		this.setFocusable(true);     // for keylistener
+		this.addKeyListener(this);
+	}
+
+	// method: paintComponent
+	// description: This method will paint the items onto the graphics panel.  This method is called when the panel is
+	//   first rendered.  It can also be called by this.repaint(). You'll want to draw each of your objects.
+	// This is the only place that you can draw objects.
+	// parameters: Graphics g - This object is used to draw your images onto the graphics panel.
+	public void paintComponent(Graphics g){
+		Graphics2D g2 = (Graphics2D) g;
+
+		background1.draw(this, g);
+		background2.draw(this, g);
+
+		item.draw(g2, this);
+		sprite.draw(g2, this);
+		for (int i =0; i<cans.size();i++) {
+			cans.get(i).draw(g2, this);
+		}
+		g2.setColor(Color.RED);
+		Rectangle r = sprite.getBounds();
+		g2.draw(r);
+
+
+
+
+
+	}
+
+	// method:clock
+	// description: This method is called by the clocklistener every 5 milliseconds.  You should update the coordinates
+	// of one of your characters in this method so that it moves as time changes.  After you update the
+	// coordinates you should repaint the panel.
+	public void clock(){
+		// You can move any of your objects by calling their move methods.
+		sprite.move(this);
+
+		background1.move();
+		background2.move();
+
+		counter++;
+		if (counter%1000==0) {
+			cans.add(new Item(background1.getWidth(), (int)(Math.random()*background1.getHeight()) + 1, "images/objects/box.png", 4));
+		}
+
+		for (int i = 0; i <cans.size();i++) {
+
+			cans.get(i).move(this);
+		}
+		// You can also check to see if two objects intersect like this. In this case if the sprite collides with the
+		// item, the item will get smaller.
+		if(sprite.collision(item) && sprite.getY() < item.getY()) {
+			System.out.println("stop");
+		}
+		if (mover) {
+			sprite.walkRight();
+		}
+		
+		sprite.run();
+		sprite.x_coordinate-=5;
+		if(sprite.upPressed&&sprite.y_coordinate>0)
+			sprite.y_coordinate-=2;
+		if(sprite.downPressed&&sprite.y_coordinate<200)
+			sprite.y_coordinate+=2;
+		this.repaint();
+	}
+
+	// method: keyPressed()
+	// description: This method is called when a key is pressed. You can determine which key is pressed using the
+	// KeyEvent object.  For example if(e.getKeyCode() == KeyEvent.VK_LEFT) would test to see if
+	// the left key was pressed.
+	// parameters: KeyEvent e
+	@Override
+	public void keyPressed(KeyEvent e) {
+
+
+
+		if(e.getKeyCode() == KeyEvent.VK_UP) {
+			sprite.upPressed = true;
+			
+		}
+		else if(e.getKeyCode() == KeyEvent.VK_DOWN && !(sprite.collision(item) && sprite.getY() < item.getY()))
+			sprite.downPressed = true;
+		else if(e.getKeyCode() == KeyEvent.VK_SPACE)
+			sprite.run();
+		else if(e.getKeyCode() == KeyEvent.VK_J)
+			sprite.jump();
+		else if(e.getKeyCode() == KeyEvent.VK_D) {
+			playSound("src/sounds/bump.WAV");
+			sprite.die();
+		}
+	}
+
+	// This function will play the sound "fileName".
+	public static void playSound(String fileName) {
+		try {
+			File url = new File(fileName);
+			Clip clip = AudioSystem.getClip();
+
+			AudioInputStream ais = AudioSystem.getAudioInputStream(url);
+			clip.open(ais);
+			clip.start();
+		}
+		catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+
+	// method: keyTyped()
+	// description: This method is called when a key is pressed and released. It basically combines the keyPressed and
+	//              keyReleased functions.  You can determine which key is typed using the KeyEvent object.  
+	// For example if(e.getKeyCode() == KeyEvent.VK_LEFT) would test to see if the left key was typed.
+	// You probably don't want to do much in this method, but instead want to implement the keyPresses and keyReleased methods.
+	// parameters: KeyEvent e
+	@Override
+	public void keyTyped(KeyEvent e) {
+
+
+	}
+
+	// method: keyReleased()
+	// description: This method is called when a key is released. You can determine which key is released using the
+	// KeyEvent object.  For example if(e.getKeyCode() == KeyEvent.VK_LEFT) would test to see if
+	// the left key was pressed.
+	// parameters: KeyEvent e
+	@Override
+	public void keyReleased(KeyEvent e) {
+
+		if(e.getKeyCode() ==  KeyEvent.VK_UP)
+			sprite.upPressed = false;
+		if(e.getKeyCode() ==  KeyEvent.VK_DOWN)
+			sprite.downPressed = false;
+		else if(e.getKeyCode() ==  KeyEvent.VK_SPACE)
+			sprite.slowDown();
+
+	}
+
 }
-
-// method: paintComponent
-// description: This method will paint the items onto the graphics panel.  This method is called when the panel is
-//   first rendered.  It can also be called by this.repaint(). You'll want to draw each of your objects.
-// This is the only place that you can draw objects.
-// parameters: Graphics g - This object is used to draw your images onto the graphics panel.
-public void paintComponent(Graphics g){
-Graphics2D g2 = (Graphics2D) g;
-
-background1.draw(this, g);
-background2.draw(this, g);
-
-item.draw(g2, this);
-sprite.draw(g2, this);
-for (int i =0; i<cans.size();i++) {
-cans.get(i).draw(g2, this);
-}
-g2.setColor(Color.RED);
-Rectangle r = sprite.getBounds();
-g2.draw(r);
-
-
-
-
-
-}
-
-// method:clock
-// description: This method is called by the clocklistener every 5 milliseconds.  You should update the coordinates
-// of one of your characters in this method so that it moves as time changes.  After you update the
-// coordinates you should repaint the panel.
-public void clock(){
-// You can move any of your objects by calling their move methods.
-sprite.move(this);
-
-background1.move();
-background2.move();
-
-counter++;
-if (counter%1000==0) {
-cans.add(new Item(background1.getWidth(), (int)(Math.random()*background1.getHeight()) + 1, "images/objects/box.png", 4));
-}
-
-for (int i = 0; i <cans.size();i++) {
-
-cans.get(i).move(this);
-}
-// You can also check to see if two objects intersect like this. In this case if the sprite collides with the
-// item, the item will get smaller.
-if(sprite.collision(item) && sprite.getY() < item.getY()) {
-System.out.println("stop");
-sprite.stop_Vertical();
-}
-if (mover) {
-sprite.walkRight();
-}
-this.repaint();
-}
-
-// method: keyPressed()
-// description: This method is called when a key is pressed. You can determine which key is pressed using the
-// KeyEvent object.  For example if(e.getKeyCode() == KeyEvent.VK_LEFT) would test to see if
-// the left key was pressed.
-// parameters: KeyEvent e
-@Override
-public void keyPressed(KeyEvent e) {
-
-
-
-if(e.getKeyCode() == KeyEvent.VK_UP) {
-sprite.moveUp();
-
-}
-else if(e.getKeyCode() == KeyEvent.VK_DOWN && !(sprite.collision(item) && sprite.getY() < item.getY()))
-sprite.moveDown();
-else if(e.getKeyCode() == KeyEvent.VK_SPACE)
-sprite.run();
-else if(e.getKeyCode() == KeyEvent.VK_J)
-sprite.jump();
-else if(e.getKeyCode() == KeyEvent.VK_D) {
-playSound("src/sounds/bump.WAV");
-sprite.die();
-}
-}
-
-// This function will play the sound "fileName".
-public static void playSound(String fileName) {
-try {
-File url = new File(fileName);
-Clip clip = AudioSystem.getClip();
-
-AudioInputStream ais = AudioSystem.getAudioInputStream(url);
-clip.open(ais);
-clip.start();
-}
-catch (Exception ex) {
-ex.printStackTrace();
-}
-}
-
-// method: keyTyped()
-// description: This method is called when a key is pressed and released. It basically combines the keyPressed and
-//              keyReleased functions.  You can determine which key is typed using the KeyEvent object.  
-// For example if(e.getKeyCode() == KeyEvent.VK_LEFT) would test to see if the left key was typed.
-// You probably don't want to do much in this method, but instead want to implement the keyPresses and keyReleased methods.
-// parameters: KeyEvent e
-@Override
-public void keyTyped(KeyEvent e) {
-
-
-}
-
-// method: keyReleased()
-// description: This method is called when a key is released. You can determine which key is released using the
-// KeyEvent object.  For example if(e.getKeyCode() == KeyEvent.VK_LEFT) would test to see if
-// the left key was pressed.
-// parameters: KeyEvent e
-@Override
-public void keyReleased(KeyEvent e) {
-
-if(e.getKeyCode() == KeyEvent.VK_RIGHT || e.getKeyCode() == KeyEvent.VK_LEFT)
-sprite.idle();
-else if(e.getKeyCode() ==  KeyEvent.VK_UP || e.getKeyCode() ==  KeyEvent.VK_DOWN)
-sprite.stop_Vertical();
-else if(e.getKeyCode() ==  KeyEvent.VK_SPACE)
-sprite.slowDown();
-
-}
-
-}
-
-
